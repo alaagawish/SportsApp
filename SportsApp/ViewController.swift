6764d6ccc449521b1ba3e0d03d1ec0e2ed64327c
Alaa Gawish <alaagawish33@gmail.com>
2023-05-17 20:51:02 +0300

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Alaa on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
