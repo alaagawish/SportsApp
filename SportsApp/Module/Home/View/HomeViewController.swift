4fbf53455c6e9326ea45b70d1c4fb2576b68a124
Alaa Gawish <alaagawish33@gmail.com>
2023-05-19 21:04:37 +0300

    home screen

---
+++ SportsApp/Module/Home/View/HomeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Alaa on 17/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    let sports = [Sport(name: "Football", image: "football"),
+                    Sport(name: "Basketball", image: "basketball"),
+                    Sport(name: "Cricket", image: "cricket"),
+                    Sport(name: "Tennis", image: "tennis")]
+    @IBOutlet weak var myCollection: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sports.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sportCell", for: indexPath) as! SportCollectionViewCell
+        
+        cell.sportImg.image = UIImage(named: sports[indexPath.row].image)
+        cell.sportName.text = sports[indexPath.row].name
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width * 0.499, height: self.view.frame.height * 0.24)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.1
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(sports[indexPath.row].name)
+        
+    }
+
+}
+
