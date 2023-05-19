70701844cca4da1bc1b6643a8f2b3744c332b241
Alaa Gawish <alaagawish33@gmail.com>
2023-05-19 16:01:25 +0300

    getting response using alamofire

---
+++ SportsApp/Model/MyResponse.swift
@@ -0,0 +1,14 @@
+//
+//  MyResponse.swift
+//  SportsApp
+//
+//  Created by Alaa on 19/05/2023.
+//
+
+import Foundation
+
+
+struct MyResponse: Decodable{
+    let success: Int
+    let result: [League]
+}
