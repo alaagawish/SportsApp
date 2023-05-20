e6701428c66e80c4baddf8c1724fc25cf154682b
Alaa Gawish <alaagawish33@gmail.com>
2023-05-20 21:24:58 +0300

    creating core data to save favourites

---
+++ SportsApp/Service/LocalSource/LocalSource.swift
@@ -0,0 +1,98 @@
+//
+//  LocalSource.swift
+//  SportsApp
+//
+//  Created by Alaa on 20/05/2023.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class LocalSource{
+        
+    var managedContext: NSManagedObjectContext
+    init(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+         managedContext = appDelegate.persistentContainer.viewContext
+        
+    }
+    func insertLeague(l: LeagueLocal){
+        let entity = NSEntityDescription.entity(forEntityName: "League", in: managedContext)
+        let league = NSManagedObject(entity: entity!, insertInto: managedContext)
+        league.setValue(l.name, forKey: "name")
+        league.setValue(l.youtube, forKey: "youtube")
+        league.setValue(l.sport, forKey: "sport")
+        league.setValue(l.logo, forKey: "logo")
+        league.setValue(l.key, forKey: "key")
+        do{
+            try managedContext.save()
+            print("\nInserting a league done...\n")
+        }catch let error as NSError{
+            print("\nerror in adding to favourite: \(error)\n")
+        }
+    }
+    
+    func getDataFromLocal() -> [LeagueLocal]{
+        var leaguesL: [LeagueLocal] = []
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "League")
+        do{
+            let leagues = try managedContext.fetch(fetchRequest)
+            for i in leagues{
+                let l = LeagueLocal(sport: i.value(forKey: "sport") as! String,
+                                    youtube: i.value(forKey: "youtube") as! String,
+                                    name: i.value(forKey: "name") as! String,
+                                    logo: i.value(forKey: "logo") as! String,
+                                    key: i.value(forKey: "key") as! Int)
+                leaguesL.append(l)
+            }
+            print("\nGetting all leagues done...\n")
+        }catch let error as NSError{
+            print("\nerror in fetching all leagues: \(error)\n")
+        }
+        
+        return leaguesL
+    }
+    
+    func getLeagueFromLocal(name: String) -> LeagueLocal?{
+        var leagueL: LeagueLocal?
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "League")
+        
+        let myPredicate = NSPredicate(format: "name == %@", name)
+        fetchRequest.predicate = myPredicate
+        do{
+            let leagues = try managedContext.fetch(fetchRequest)
+            let i = leagues[0]
+            leagueL = LeagueLocal(sport: i.value(forKey: "sport") as! String,
+                                      youtube: i.value(forKey: "youtube") as! String,
+                                      name: i.value(forKey: "name") as! String ,
+                                      logo: i.value(forKey: "logo") as! String,
+                                      key: i.value(forKey: "key") as! Int)
+            print("\nGetting league done...\n")
+        }catch let error as NSError{
+            print("\nerror in fetching all leagues: \(error)\n")
+        }
+        
+        return leagueL ?? nil
+    }
+    
+    func getDeleteFromLocal(name: String) {
+       
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "League")
+        
+        let myPredicate = NSPredicate(format: "name == %@", name)
+        fetchRequest.predicate = myPredicate
+        do{
+            let leagues = try managedContext.fetch(fetchRequest)
+            managedContext.delete(leagues[0])
+            try managedContext.save()
+            print("\nDelete league done...\n")
+               
+        }catch let error as NSError{
+            print("\nerror in deleteting a league : \(error)\n")
+        }
+        
+       
+    }
+ 
+}
